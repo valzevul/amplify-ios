c0b2047db7caa5742eca85a4a39c94972262c750
Diogo Antunes <diogo.antunes@gmail.com>
2023-06-30 14:40:24 +0100

    Timezone extraction

--- Amplify/Categories/DataStore/Model/Temporal/Date.swift
+++ Amplify/Categories/DataStore/Model/Temporal/Date.swift
@@ -18,19 +18,11 @@ extension Temporal {
   ///
   ///  - Note: `.medium`, `.long`, and `.full` are the same date format.
   public struct Date: TemporalSpec {
-    // Inherits documentation from `TemporalSpec`
-    public let foundationDate: Foundation.Date
     
     public var localTimezone: TimeZone?
     
-    public init(iso8601String: String) throws {
-      guard let date = Temporal.Date.iso8601Date(from: iso8601String) else {
-        throw DataStoreError.invalidDateFormat(iso8601String)
-      }
-      
-      self.init(date)
-      localTimezone = TimeZone(iso8601String: iso8601String)
-    }
+    // Inherits documentation from `TemporalSpec`
+    public let foundationDate: Foundation.Date
     
     // Inherits documentation from `TemporalSpec`
     public static func now() -> Self {
@@ -42,9 +34,43 @@ extension Temporal {
       self.foundationDate = Temporal
         .iso8601Calendar
         .startOfDay(for: date)
+      
+      // We don't care 'cause we aint supporting lower!!!!
+      if #available(iOS 15.0, *) {
+        localTimezone = TimeZone(iso8601: foundationDate.formatted(.iso8601))
+      }
     }
   }
 }
 
 // Allow date unit operations on `Temporal.Date`
 extension Temporal.Date: DateUnitOperable {}
+extension TimeZone {
+  init?(iso8601: String) {
+    let tz = iso8601.dropFirst(19) // remove yyyy-MM-ddTHH:mm:ss part
+    if tz == "Z" {
+      self.init(secondsFromGMT: 0)
+    } else if tz.count == 3 { // assume +/-HH
+      if let hour = Int(tz) {
+        self.init(secondsFromGMT: hour * 3600)
+        return
+      }
+    } else if tz.count == 5 { // assume +/-HHMM
+      if let hour = Int(tz.dropLast(2)), var min = Int(tz.dropFirst(3)) {
+        min = (hour < 0) ? -1 * min : min
+        self.init(secondsFromGMT: (hour * 60 + min) * 60)
+        return
+      }
+    } else if tz.count == 6 { // assime +/-HH:MM
+      let parts = tz.components(separatedBy: ":")
+      if parts.count == 2 {
+        if let hour = Int(parts[0]), var min = Int(parts[1]) {
+          min = (hour < 0) ? -1 * min : min
+          self.init(secondsFromGMT: (hour * 60 + min) * 60)
+          return
+        }
+      }
+    }
+    return nil
+  }
+}
